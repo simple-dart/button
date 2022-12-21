c128368f6819f412ac750321970d3d9ea8d8be32
viktorzaharov <viktorzahar@gmail.com>
2022-12-21 09:37:49 +0300

    Version 0.0.2

--- lib/simple_dart_button.dart
+++ lib/simple_dart_button.dart
@@ -4,9 +4,8 @@ import 'package:simple_dart_ui_core/simple_dart_ui_core.dart';
 
 class Button extends Component with MixinActive, MixinDisable {
   Button() : super('Button') {
-    element.style
-      ..alignItems = 'center'
-      ..justifyContent = 'center';
+    vAlign = Align.center;
+    hAlign = Align.center;
   }
 
   Stream<MouseEvent> get onClick => element.onClick.where((event) => !disabled);
